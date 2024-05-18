b9a24060cef8218df2ac665d4ddf760a434a5005
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2024-05-18 18:04:41 +0530

    question 2

--- JSON and Object Manipulation/script.js
+++ JSON and Object Manipulation/script.js
@@ -74,36 +74,116 @@
 // let strinValue = JSON.stringify(greekycommunityMembers, replacerFunction, 20);
 // console.log(strinValue);
 
-//create a function to Convert a JavaScript Value to JSON String
+//1.create a function to Convert a JavaScript Value to JSON String
 
-let bankDetails = {
-  id: "csyui",
-  lenderProfileId: "ide=212",
-  bankName: "cync-blr",
-  bankId: "1234567890",
-  branchName: "cync-blr",
-  branchId: "1234567890",
-  accountNumber: "1234567890",
-  ifscCode: "1234567890",
-};
+// let bankDetails = {
+//   id: "csyui",
+//   lenderProfileId: "ide=212",
+//   bankName: "cync-blr",
+//   bankId: "1234567890",
+//   branchName: "cync-blr",
+//   branchId: "1234567890",
+//   accountNumber: "1234567890",
+//   ifscCode: "1234567890",
+// };
 
-let bankDetailsDuplicate = {
-  id: "csyui",
-  lenderProfileId: "ide=212",
-  bankName: "cync-blr",
-  bankId: "1234567890",
-  branchName: "cync-blr",
-  branchId: "1234567890",
-  accountNumber: "1234567890",
-  ifscCode: "1234567890",
-};
+// let bankDetailsDuplicate = {
+//   id: "csyui",
+//   lenderProfileId: "ide=212",
+//   bankName: "cync-blr",
+//   bankId: "1234567890",
+//   branchName: "cync-blr",
+//   branchId: "1234567890",
+//   accountNumber: "1234567890",
+//   ifscCode: "1234567890",
+// };
 
-console.log(bankDetails);
-function covertToJson(value) {
-  return JSON.stringify(value, null, 4);
+// console.log(bankDetails);
+// function covertToJson(value) {
+//   return JSON.stringify(value, null, 4);
+// }
+
+// let value = covertToJson(bankDetails);
+// let valueClone = covertToJson(bankDetailsDuplicate);
+// console.log(value);
+// console.log(value == valueClone);
+
+//2.create a function to Modify Object Values and Convert to JSON String:
+
+// let users = [
+//   {
+//     name: "Abhi",
+//     isActive: true,
+//     numberOfSessionsAttended: 3,
+//     id: 121,
+//   },
+//   {
+//     name: "John",
+//     isActive: false,
+//     numberOfSessionsAttended: 5,
+//     id: 122,
+//   },
+//   {
+//     name: "Jane",
+//     isActive: true,
+//     numberOfSessionsAttended: 2,
+//     id: 123,
+//   },
+// ];
+
+// let users = {
+//   user1: {
+//     name: "Abhi",
+//     isActive: true,
+//     numberOfSessionsAttended: 3,
+//     id: 121,
+//   },
+//   user2: {
+//     name: "John",
+//     isActive: false,
+//     numberOfSessionsAttended: 5,
+//     id: 122,
+//   },
+//   user3: {
+//     name: "Jane",
+//     isActive: true,
+//     numberOfSessionsAttended: 2,
+//     id: 123,
+//   },
+// };
+
+// function replacerFunction(key, value) {
+//   if (Array.isArray(value)) {
+//     value.forEach((ele, index) => {
+//         ele.name="IN" + ele.name;
+//       //    return (ele["name"]);
+//     });
+//     // return value
+//   }
+//   return value;
+// }
+
+function replacerFunction(key, value) {
+  if (typeof value === "object" && value !== null) {
+    let modifiedObject = {};
+    for (let prop in value) {
+      if (
+        typeof value[prop] === "object" &&
+        value[prop] !== null &&
+        value[prop].hasOwnProperty("name")
+      ) {
+        modifiedObject[prop] = {
+          ...value[prop], // Copy existing properties
+          name: "In " + value[prop].name, // Modify the 'name' property
+        };
+      } else {
+        modifiedObject[prop] = value[prop];
+      }
+    }
+    return modifiedObject;
+  }
+  return value;
 }
 
-let value = covertToJson(bankDetails);
-let valueClone = covertToJson(bankDetailsDuplicate);
-console.log(value);
-console.log(value == valueClone);
+const strigigyValue = JSON.stringify(users, replacerFunction, 4);
+console.log(strigigyValue);
